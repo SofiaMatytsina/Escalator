5e631b3dd30d3a0655193ee9f3d1ce167e28d148
SofiaMatytsina <74772375+SofiaMatytsina@users.noreply.github.com>
2021-02-24 01:24:01 +0300

    Text
    Изменяемый текст

---
+++ eskalator_latest/Assets/WritingText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WritingText : MonoBehaviour
+{
+    public Text nesessaryText;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            WriteText();
+        }
+    }
+
+    public void WriteText()
+    {
+        nesessaryText.text = "Pulse or something";
+    }
+}
